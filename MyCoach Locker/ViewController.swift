40f0f0563b270bf81207a5605b9ca22de3cf01f6
RomainRui <romain.rui10@gmail.com>
2017-01-03 21:15:16 +0100

    Initial Commit

---
+++ MyCoach Locker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyCoach Locker
+//
+//  Created by Rplay on 03/01/2017.
+//  Copyright © 2017 rplay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
